a5de2a46b05c5a172384400463548685bb8bb40c
hamo6693 <hamo6693@gamil.com>
2025-01-02 00:00:28 +0300

    first commit

--- js/indexPage/sliderComponent.js
+++ js/indexPage/sliderComponent.js
@@ -1,3 +1,22 @@
+const slider = `
+  <div class="carousel-item active">
+                      <a href="article.html">
+                        <img
+                          src="./images/picture-1.jpg"
+                          class="d-block w-100"
+                          alt="..."
+                        />
+
+                        <div class="carousel-text">
+                          <h3>
+                            وريم إيبسوم هو ببساطة نص شكلي يستخدم في صناعة
+                            الطباعة والتنضيد
+                          </h3>
+                        </div>
+                      </a>
+                    </div>
+`
+
 
 class SliderComponent extends HTMLElement {
     
@@ -6,7 +25,7 @@ class SliderComponent extends HTMLElement {
     }
 
     connectedCallback() {
-        this.innerHTML = `<h1> welcome word  </h1>`
+        this.innerHTML = slider
         this.querySelector("h1").innerText = this.getAttribute("text")
     }
 } 
